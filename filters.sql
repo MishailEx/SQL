6eca1049a5a1aef50c1f1fdb2ddcc4390cc24079
MishailEx <83918375+MishailEx@users.noreply.github.com>
2021-10-12 08:33:19 +0300

    Add files via upload

--- filters.sql
+++ filters.sql
@@ -1,4 +1,4 @@
-select * from product where type_id = 1;
+select * from product where product.name like '%сыр%';
 
 select * from product where name like '%мороженное%';
 
@@ -9,7 +9,7 @@ select * from product order by price desc limit 1;
 select type.name, count(type.id) from product inner join type on product.type_id = type.id
 group by type.name;
 
-select * from product where type_id = 1 or type_id = 3;
+select * from product where product.name like '%сыр%' or product.name like '%молоко%';
 
 select type.name, count(type.id)
 from product
@@ -18,6 +18,3 @@ group by type.name
 having count(type.id) < 10;
 
 select product.name, type.name from product inner join type on product.type_id = type.id;
-
-
-
